fa985763bf0c6434deb723e0000219ec87ea43ff
nobody <hugosum.dev@protonmail.com>
2025-01-06 09:48:10 +0000

    update flake

--- flake.nix
+++ flake.nix
@@ -3,11 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     nur.url = "github:nix-community/NUR/master";
     nur.inputs.nixpkgs.follows = "nixpkgs";
+    # nur.inputs.flake-parts.follows = "flake-parts";
   };
 
   outputs = { self, nixpkgs }:
