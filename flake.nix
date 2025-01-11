1257cba0d7e030bd1c98a98d708d8d6e967e3745
nobody <hugosum.dev@protonmail.com>
2025-01-11 07:51:50 +0000

    update input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "proxy flake for controlling input for all my flakes";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -12,7 +12,6 @@
 
     nur.url = "github:nix-community/NUR/master";
     nur.inputs.nixpkgs.follows = "nixpkgs";
-    # nur.inputs.flake-parts.follows = "flake-parts";
   };
 
   outputs = { self, nixpkgs }:
